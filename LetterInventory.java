21191b4be41cc7f58754e41f864fa618f4f098f9
Tuan Le <122320817+vozilla@users.noreply.github.com>
2023-02-10 23:12:59 -0800

    Add files via upload

---
+++ LetterInventory.java
@@ -0,0 +1,10 @@
+public class LetterInventory {
+    private int x;
+    public LetterInventory(int y) {
+        x = y;
+    }
+    public void mainMethod() {
+        LetterInventory myObj = new LetterInventory(5);
+        System.out.println(myObj.x);
+    }
+}
---
+++ LetterInventoryTest.java
@@ -0,0 +1,6 @@
+public class LetterInventoryTest {
+    public static void main(String[] args) {
+        LetterInventory test = new LetterInventory();
+        test.mainMethod();
+    }
+}
